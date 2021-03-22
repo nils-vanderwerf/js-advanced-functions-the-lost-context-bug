fdd9345df2c846db88a270f9dc088d01808fe3fe
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-03-22 20:38:53 +1100

    Added js file

---
+++ index.js
@@ -0,0 +1,28 @@
+let configuration = {
+    frontContent: "Happy Birthday, Odin One-Eye!",
+    insideContent: "From Asgard to Nifelheim, you're the best all-father ever.\n\nLove,",
+    closing: {
+        "Thor": "Admiration, respect, and love",
+        "Loki": "Your son"
+    },
+    signatories: [
+        "Thor",
+        "Loki"
+    ]
+}
+
+let printCard = function() {
+    console.log(`Console message: ${this.frontContent}`)
+    console.log(this.insideContent)
+
+    let outerContext = this
+
+    this.signatories.forEach(function(signatory){
+        let message = `${outerContext.closing[signatory]}, ${signatory}`
+        console.log(message)
+    })
+}
+
+    this.signatories.forEach(contextBoundForEachExpr)
+    printCard.call(configuration);
+
